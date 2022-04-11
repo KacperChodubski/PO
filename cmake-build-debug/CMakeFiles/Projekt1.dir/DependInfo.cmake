e4647c2e2dd9fe2fcf1f852a61817e30c61b1ad1
Kacper <kaper02@o2.pl>
2022-04-11 23:24:51 +0200

    cos

---
+++ cmake-build-debug/CMakeFiles/Projekt1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mojskarb/obiektowka/Projekt1/Animal.cpp" "CMakeFiles/Projekt1.dir/Animal.cpp.o" "gcc" "CMakeFiles/Projekt1.dir/Animal.cpp.o.d"
+  "/Users/mojskarb/obiektowka/Projekt1/Organism.cpp" "CMakeFiles/Projekt1.dir/Organism.cpp.o" "gcc" "CMakeFiles/Projekt1.dir/Organism.cpp.o.d"
+  "/Users/mojskarb/obiektowka/Projekt1/OrganismList.cpp" "CMakeFiles/Projekt1.dir/OrganismList.cpp.o" "gcc" "CMakeFiles/Projekt1.dir/OrganismList.cpp.o.d"
+  "/Users/mojskarb/obiektowka/Projekt1/Plant.cpp" "CMakeFiles/Projekt1.dir/Plant.cpp.o" "gcc" "CMakeFiles/Projekt1.dir/Plant.cpp.o.d"
+  "/Users/mojskarb/obiektowka/Projekt1/Wolf.cpp" "CMakeFiles/Projekt1.dir/Wolf.cpp.o" "gcc" "CMakeFiles/Projekt1.dir/Wolf.cpp.o.d"
+  "/Users/mojskarb/obiektowka/Projekt1/World.cpp" "CMakeFiles/Projekt1.dir/World.cpp.o" "gcc" "CMakeFiles/Projekt1.dir/World.cpp.o.d"
+  "/Users/mojskarb/obiektowka/Projekt1/main.cpp" "CMakeFiles/Projekt1.dir/main.cpp.o" "gcc" "CMakeFiles/Projekt1.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Projekt1.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projekt1.dir/Animal.cpp.o"
+  "CMakeFiles/Projekt1.dir/Animal.cpp.o.d"
+  "CMakeFiles/Projekt1.dir/Organism.cpp.o"
+  "CMakeFiles/Projekt1.dir/Organism.cpp.o.d"
+  "CMakeFiles/Projekt1.dir/OrganismList.cpp.o"
+  "CMakeFiles/Projekt1.dir/OrganismList.cpp.o.d"
+  "CMakeFiles/Projekt1.dir/Plant.cpp.o"
+  "CMakeFiles/Projekt1.dir/Plant.cpp.o.d"
+  "CMakeFiles/Projekt1.dir/Wolf.cpp.o"
+  "CMakeFiles/Projekt1.dir/Wolf.cpp.o.d"
+  "CMakeFiles/Projekt1.dir/World.cpp.o"
+  "CMakeFiles/Projekt1.dir/World.cpp.o.d"
+  "CMakeFiles/Projekt1.dir/main.cpp.o"
+  "CMakeFiles/Projekt1.dir/main.cpp.o.d"
+  "Projekt1"
+  "Projekt1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projekt1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
